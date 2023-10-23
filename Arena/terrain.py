c9614a4405c7fcb92d7fbd7c61f7ad696504373f
Ludwig-Uni <ludwig.kolesch@student.uni-tuebingen.de>
2023-10-23 11:22:33 +0200

    Fix linter errors

--- Arena/terrain.py
+++ Arena/terrain.py
@@ -4,23 +4,27 @@ import pygame
 
 class Terrain():
     def __init__(self) -> None:
-        self.size = Vector2(50,50)
+        self.size = Vector2(50, 50)
+
 
 class Grass(Terrain):
     def __init__(self) -> None:
         super().__init__()
         self.color = pygame.Color('#8BC34A')
-        
+
+
 class Stone(Terrain):
     def __init__(self) -> None:
         super().__init__()
         self.color = pygame.Color('#9E9E9E')
-        
+
+
 class Water(Terrain):
     def __init__(self) -> None:
         super().__init__()
         self.color = pygame.Color('#1976D2')
-        
+
+
 class Mud(Terrain):
     def __init__(self) -> None:
         super().__init__()
