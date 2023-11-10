9d2f944232ec5539f9665f40c778303ca271fb5b
Philip-Daniel Ebsworth <84278185+Phil-Ebsworth@users.noreply.github.com>
2023-11-10 15:33:12 +0100

    added level2.tmx and playertile

--- Game/robot.py
+++ Game/robot.py
@@ -1,16 +1,18 @@
 from pygame import Vector2, Surface
 import pygame
+import math
 
 
 class BasicRobot():
     '''The BasicRobot class represents a playable basic robot
 with a position, radius and direction.'''
-    def __init__(self, gameState, x, y, color) -> None:
+    def __init__(self, gameState, x, y, image) -> None:
         self.gameState = gameState
+        self.cellSize = gameState.tileSize
         self.position = Vector2(x, y)  # Position in coordinates
         self.radius = 25  # Radius in pixels
         self.direction = 0  # Angle of orientation, direction ∊ (-180, 180]
-        self.color = color  # RGB color of the robot
+        self.image = pygame.image.load(image)  # RGB color of the robot
 
     def move(self,
              movementVector: Vector2,
@@ -32,15 +34,25 @@ with a position, radius and direction.'''
 
     def draw(self, surface: Surface, selected: bool):
         """Draws the robot on the `surface`"""
-        pygame.draw.circle(surface, self.color, self.position, self.radius)
+        spritePoint = self.position
+        texturePoint = Vector2(0, 0).elementwise()*self.cellSize
+        textureRect = pygame.Rect(int(texturePoint.x), int(texturePoint.y), int(self.cellSize.x),int(self.cellSize.y))
+        image = self.image.subsurface(textureRect)
+        if self.direction == 0:
+            surface.blit(image, spritePoint)
+        else:
+            rotatedImage = pygame.transform.rotate(image, -self.direction)
+            spritePoint.x += (image.get_width() - image.get_width()) // 2
+            spritePoint.y -= (image.get_height() - image.get_height()) // 2
+            surface.blit(rotatedImage, spritePoint)
         if selected:
-            pygame.draw.circle(surface, 'blue', self.position, self.radius, 5)
+            pygame.draw.circle(surface, 'red', self.position, 2, 5)
 
-        lineVector = Vector2.from_polar((self.radius, self.direction))
+        """ lineVector = Vector2.from_polar((self.radius, self.direction))
         pygame.draw.line(surface,
-                         (0, 0, 0),
-                         self.position,
-                         self.position + lineVector)
+                        (0, 0, 0),
+                        self.position,
+                        self.position + lineVector) """
 
 
 def clamp(x: int, minimum: int, maximum: int) -> int:
