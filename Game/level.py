1c6e0da5d91500e6073e5bb5745f72047eec64ac
Philip-Daniel Ebsworth <84278185+Phil-Ebsworth@users.noreply.github.com>
2023-11-15 19:12:18 +0100

    solved layer tileset resolve

--- Game/level.py
+++ Game/level.py
@@ -2,7 +2,22 @@ from pygame import Vector2
 import tmx
 
 from Game.Entities.robot import BasicRobot
-
+def decodeLayer(tileMap,layer):
+        gid = None
+        for tile in layer.tiles:
+            if tile.gid != 0:
+                gid = tile.gid
+                break
+        if gid is None:
+            tileset = tileMap.tilesets[0]
+        else:
+            tileset = None
+            for t in tileMap.tilesets:
+                if gid >= t.firstgid and gid < t.firstgid+t.tilecount:
+                    tileset = t
+                    break
+            
+        return tileset
 
 def decodeUnitsLayer(state,tileMap):
         """
@@ -11,23 +26,24 @@ def decodeUnitsLayer(state,tileMap):
         units = []
         selected = True
         count = 1
-        for l in range(4,6):      
-            layer = tileMap.layers[l]
-            tileset = tileMap.tilesets[l]
-            source = tileset.image.source
-            for y in range(tileMap.height):
-                for x in range(tileMap.width):
-                    tile = layer.tiles[x + y*tileMap.width]
-                    if tile.gid == 0:
-                        continue
-                    lid = tile.gid - tileset.firstgid
-                    tileX = lid % tileset.columns
-                    tileY = lid // tileset.columns
-                    unit = BasicRobot(state,source,Vector2(x*tileMap.height,y*tileMap.width),Vector2(tileX,tileY),count,selected)
-                    #only sets firs player to true
-                    count += 1
-                    if selected:
-                        selected = False
-                    units.append(unit)
+        for layer in tileMap.layers: 
+            if layer.name == "Player1" or layer.name == "Player2":   
+                
+                tileset = decodeLayer(tileMap,layer)
+                source = tileset.image.source
+                for y in range(tileMap.height):
+                    for x in range(tileMap.width):
+                        tile = layer.tiles[x + y*tileMap.width]
+                        if tile.gid == 0:
+                            continue
+                        lid = tile.gid - tileset.firstgid
+                        tileX = lid % tileset.columns
+                        tileY = lid // tileset.columns
+                        unit = BasicRobot(state,source,Vector2(x*tileMap.height,y*tileMap.width),Vector2(tileX,tileY),count,selected)
+                        #only sets firs player to true
+                        count += 1
+                        if selected:
+                            selected = False
+                        units.append(unit)
 
         return units
